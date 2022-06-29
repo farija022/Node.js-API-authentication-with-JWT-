15203ddddff5f6e8ccb1283f166cb4db15368ced
FarijaAkter <farija022@gmail.com>
2022-06-29 13:00:49 +0600

    updated

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
+const verify = require("./verifyToken")
 
+router.get("/", verify, (req, res) => {
+    // res.json({
+    //     posts: {
+    //         title: "Farija",
+    //         description: "Random data you should not access"
+    //     }
+    // });
 
-router.get("/", (req, res) => {
-    res.json({
-        posts: {
-            title: "Farija",
-            description: "Random data you should not access"
-        }
-    });
+    res.send(req.user);
 });
 
 module.exports = router;
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,13 +1,14 @@
-const jwt = require('jwtwebtoken');
+const jwt = require('jsonwebtoken');
 
-//MiddleWare function
-function auth(req, res, next) {
+//MiddleWare function for private data 
+module.exports = function (req, res, next) {
     const token = req.header('auth-token');
     if (!token) return res.status(401).send('Assess denied');
 
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET)
         req.user = verified;
+        next();
     } catch (err) {
         res.status(400).send("Invalid Token");
     }
